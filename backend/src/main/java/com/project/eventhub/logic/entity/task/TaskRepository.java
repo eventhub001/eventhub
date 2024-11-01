bde8b402ace9828312a0d1694df7768f9baa1fc0
AlexLight96 <157441793+AlexLight96@users.noreply.github.com>
2024-11-01 11:42:27 -0600

    Backend complete for Task and Task Progress

---
+++ backend/src/main/java/com/project/eventhub/logic/entity/task/TaskRepository.java
@@ -0,0 +1,10 @@
+package com.project.eventhub.logic.entity.task;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TaskRepository extends JpaRepository<Task, Integer> {
+
+
+
+
+}
